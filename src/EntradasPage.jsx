8a6c15ab513227b6fad7d2ef7ef0d4b8b2f831a3
lauragonzalezcomalada <lauragonzalezcomalada@gmail.com>
2025-09-04 14:52:57 -0300

    possible correccio

--- src/EntradasPage.jsx
+++ src/EntradasPage.jsx
@@ -213,8 +213,8 @@ export default function EntradasPage() {
     if (Object.keys(referencias || {}).length === 0) {
         return (
             <div className="d-flex justify-content-center align-items-center" style={{ marginTop: '56px', height: '100vh' }}>
-                
-                <img src={ojitosgif} style={{height:'250px', width:'250px'}} />
+
+                <img src={ojitosgif} style={{ height: '250px', width: '250px' }} />
                 {/*<div className="spinner-border text-success" role="status">
                   <span className="visually-hidden">Loading...</span>
                 </div>*/}
@@ -225,8 +225,8 @@ export default function EntradasPage() {
     return <div style={{ marginTop: '56px', width: '100%', minHeight: '100vh', overflowY: 'auto' }}>
         {Object.entries(referencias)?.map(([fecha, listaEntradas]) => {
             console.log('fecha: ', fecha)
-            console.log('listaEntradas: ',listaEntradas)
-        return (<div> <div style={{
+            console.log('listaEntradas: ', listaEntradas)
+            return (<div> <div style={{
                 display: 'flex',
                 alignItems: 'center',
                 margin: '2rem 2rem'
@@ -419,13 +419,12 @@ export default function EntradasPage() {
                                 {/*Evento gratis sin reserva*/}
                                 {console.log('ahora va a printar el tracking itpo 2')}
                                 {evento.tracking_tipo === 2 && (
-
                                     <div key={'sin_resereva'} className={`col-md-8 d-flex align-items-center justify-content-center p-5`} style={{ paddingLeft: '1rem', paddingRight: '1rem', marginTop: '1rem', marginBottom: '1rem' }} >
                                         <div style={{ position: 'absolute', right: '5rem', display: 'flex', flexDirection: 'column' }}>
                                             <span className='fw-light fs-4'> Evento <span style={{ fontWeight: 'bold' }}> gratuito</span></span>
                                             <span className='fw-light fs-4'><span style={{ fontWeight: 'bold' }}> sin</span> reservas</span>
                                         </div>
-                                          {console.log('entramos dentro del tracking tipo 2')}
+                                        {console.log('entramos dentro del tracking tipo 2')}
                                         <div id="views-block" style={{ display: "flex", flexDirection: "row", marginLeft: '2rem', justifyContent: 'center', alignItems: 'center', color: '#491a13ff' }} >
                                             <OverlayTrigger
                                                 placement="top" // posición del tooltip
@@ -455,9 +454,9 @@ export default function EntradasPage() {
 
                                                     <Icono className="bi bi-send" style={{ fontSize: '2rem', color: '#491a13ff' }} />
                                                 </span>
-                                                </OverlayTrigger>
-                                                <span className=' px-4' style={{ fontSize: "1.5rem", fontWeight: 'light', color: '#491a13ff' }}> {evento.shares}</span>
-                                            
+                                            </OverlayTrigger>
+                                            <span className=' px-4' style={{ fontSize: "1.5rem", fontWeight: 'light', color: '#491a13ff' }}> {evento.shares}</span>
+
                                         </div>
                                         {console.log('despues del overlaytrigger2')}
 
@@ -466,7 +465,6 @@ export default function EntradasPage() {
                                 )}
                                 {/*Evento de pago con link externo*/}
                                 {evento.tracking_tipo === 3 && (
-
                                     <div key={'sin_entradas_centr'} className={`col-md-8 d-flex align-items-center justify-content-center p-3`} style={{ paddingLeft: '1rem', paddingRight: '1rem', marginTop: '1rem', marginBottom: '1rem' }} >
 
 
@@ -513,7 +511,7 @@ export default function EntradasPage() {
                                                         <span>
                                                             <Icono className="bi bi-eye-fill" style={{ fontSize: '2rem', color: '#491a13ff' }} />
                                                         </span>
-                                                       
+
                                                     </OverlayTrigger>
                                                     <span className=' px-4' style={{ fontSize: "1.5rem", fontWeight: 'light', color: '#491a13ff' }}> {evento.views}</span>
                                                 </div>
@@ -527,9 +525,7 @@ export default function EntradasPage() {
                                                             </Tooltip>
                                                         }
                                                     >   <span>
-                                                            <span>
                                                             <Icono className="bi bi-send" style={{ fontSize: '2rem', color: '#491a13ff' }} />
-                                                        </span>
                                                         </span>
                                                     </OverlayTrigger>
                                                     <span className=' px-4' style={{ fontSize: "1.5rem", fontWeight: 'light', color: '#491a13ff' }}> {evento.shares}</span>
@@ -544,9 +540,9 @@ export default function EntradasPage() {
                                                         }
                                                     >
                                                         <span>
-                                                            <span>
+
                                                             <Icono className="bi bi-hand-index" style={{ fontSize: '2rem', color: '#491a13ff' }} />
-                                                        </span>                                                        </span>
+                                                        </span>
                                                     </OverlayTrigger>
                                                     <span className=' px-4' style={{ fontSize: "1.5rem", fontWeight: 'light', color: '#491a13ff' }}> {evento.shares}</span>
                                                 </div>
@@ -555,7 +551,7 @@ export default function EntradasPage() {
                                     </div>
                                 )}
                                 {(evento.tracking_tipo === 0 || evento.tracking_tipo === 1) && (
-                         console.log('aqui tampoc entra'),
+                                    console.log('aqui tampoc entra'),
                                     <div className={`col-md-${colIcono}  d-flex align-items-center justify-content-center`} style={{ paddingRight: '3rem', flexDirection: 'column' }}>
 
                                         <div id="views-block" style={{ display: "flex", flexDirection: "row", marginLeft: '2rem', justifyContent: 'center', alignItems: 'center' }} >
@@ -603,7 +599,7 @@ export default function EntradasPage() {
                         </div >
                         {expandedEventoUuid === evento.event_uuid && (
                             console.log('aqui entra?'),
-                           
+
                             <div className="card mx-3 mb-3 p-3" style={{ backgroundColor: '#f8f9fa' }}>
                                 {loadingTickets ? (
                                     <div className="text-center">Cargando datos...</div>
